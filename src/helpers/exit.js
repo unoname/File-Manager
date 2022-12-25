608084eef1a5042133a33faff41ec89b080f63a0
aleks_pv <apanov08@gmail.com>
2022-12-25 15:00:25 +0400

    fix: correct farewellMessage

--- src/helpers/exit.js
+++ src/helpers/exit.js
@@ -1,7 +1,7 @@
 export const username = process.argv.slice(4).join('').split('=').pop()
   ? process.argv[4].split('=').pop()
   : 'User';
-const farewellMessage = `Thank you for using File Manager, ${username}!`;
+const farewellMessage = `Thank you for using File Manager, ${username} goodbye!`;
 
 export const ex = () => {
   console.log(farewellMessage);
